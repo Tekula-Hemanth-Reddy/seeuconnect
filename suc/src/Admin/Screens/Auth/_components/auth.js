23b19492694813cc84279f913214a589b8e854fa
Aditya Nalla <nallaaditya1415@gmail.com>
2020-12-21 13:45:50 +0530

    Courses added

--- suc/src/Admin/Screens/Auth/_components/auth.js
+++ suc/src/Admin/Screens/Auth/_components/auth.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {Container,Card,Form,Button,Row,Col} from 'react-bootstrap';
 import history from '../../../../history/history';
 import '../_css/auth.css';
+import * as FAIcons from 'react-icons/fa';
 import Fotter from '../../../../components/FirstPage/Fotter/_component/_fotter'
 
 class Auth extends Component{
@@ -10,8 +11,29 @@ class Auth extends Component{
         this.MPIN = React.createRef();
         this.state = {
             isSet:true,
+            passwordShown:false,
            
           };
+    }
+    showPassword()
+    {
+        if(!this.state.passwordShown)
+        {
+            this.setState(
+                {
+                    passwordShown:true,
+                }
+            );
+        }
+        else
+        {
+            this.setState(
+                {
+                    passwordShown:false,
+                }
+            );
+        }
+        
     }
     checkCredentials() {
         const mpin=Number(""+this.MPIN.current.value);
@@ -33,7 +55,7 @@ class Auth extends Component{
                                     <Form.Group>
                                         <Form.Control 
                                             size="text" 
-                                            type="password"
+                                            type={this.state.passwordShown ? "text" : "password"}
                                             required
                                             pattern="[0-9]*"
                                             id="id1"
@@ -42,6 +64,7 @@ class Auth extends Component{
                                             maxLength="6" 
                                         />
                                     </Form.Group>
+                                    <i onClick={this.showPassword.bind(this)} style={{top:"20%"}}><FAIcons.FaEye/></i>
                                 </Col>
                                 <Col xs={3}>
                                 <div className="col-lg-5 col-md-5 col-sm-5 col-xl-5" style={{marginLeft:"50%"}}>
